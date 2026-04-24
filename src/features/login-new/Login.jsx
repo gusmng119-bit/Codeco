31cd5fe11b22e559d6dede77df45dbbe5b2e28d8
Natasya Ruilla <natasya.ruilla@gmail.com>
2026-04-24 20:59:58 +0700

    feat: login page responsive

--- src/features/login-new/Login.jsx
+++ src/features/login-new/Login.jsx
@@ -1,16 +1,56 @@
 import { Col, Row } from "antd";
 import gambar1 from "../../assets/gambar1.png";
 import logo1 from "../../assets/logo1.png";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
     <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
       <Row style={{ height: "100%" }}>
-        <Col xl={12} xs={24} style={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center", padding: "40px" }}>
-          <img src={logo1} alt="Codeco Logo" style={{ maxWidth: "100%", height: "auto" }} />
+        <Col
+          xl={12}
+          xs={24}
+          style={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "white",
+          }}
+        >
+          <img
+            src={logo1}
+            alt="Codeco Logo"
+            style={{
+              width: 200,
+              height: "auto",
+              objectFit: "contain",
+              paddingBottom: 20,
+              paddingLeft: 32,
+              paddingTop: 32,
+            }}
+          />
+          <div
+            style={{
+              flex: 1,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <LoginForm />
+          </div>
         </Col>
-        <Col xl={12} xs={24} style={{ height: "100%" }}>
-          <img src={gambar1} alt="Login Preview" style={{ width: "100%", height: "100%", objectFit: "cover", display: "block" }} />
+        <Col xl={12} xs={0} style={{ height: "100%" }}>
+          <img
+            src={gambar1}
+            alt="Login Preview"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              display: "block",
+            }}
+          />
         </Col>
       </Row>
     </div>
